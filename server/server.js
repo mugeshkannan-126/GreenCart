e102085c5737a92eee3e6d5e96556dca8d66f5e6
mugeshkannan-126 <727723eucs126skcet.ac.in>
2025-05-29 15:55:12 +0530

    Completed

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhook);
 // Middleware Configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173" , credentials: true}));
+app.use(cors({origin: allowedOrigins , credentials: true}));
 
 
 app.get("/",(req, res) =>
